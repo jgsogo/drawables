848583f308004a13c002f52dd6fb495dffa30c11
jgsogo <jgsogo@gmail.com>
2022-04-23 20:41:55 +0200

    gcc failed

--- src/drawables/library.h
+++ src/drawables/library.h
@@ -11,7 +11,7 @@ namespace drawables {
     template<const char *Origin, typename TDrawList, const char *PixelsSymbol>
     class Library {
     public:
-        using BaseDrawable = BaseDrawable<Origin, TDrawList, PixelsSymbol>;
+        using BaseDrawableType = BaseDrawable<Origin, TDrawList, PixelsSymbol>;
 
         explicit Library(std::string_view name) : _name(name) {
         }
@@ -20,7 +20,7 @@ namespace drawables {
             return _name;
         }
 
-        [[nodiscard]] const std::map<std::string, std::shared_ptr<BaseDrawable>> &getDrawables() const {
+        [[nodiscard]] const std::map<std::string, std::shared_ptr<BaseDrawableType>> &getDrawables() const {
             return _drawables;
         }
 
@@ -29,7 +29,7 @@ namespace drawables {
         friend class Parser;
 
         const std::string _name;
-        std::map<std::string, std::shared_ptr<BaseDrawable>> _drawables;
+        std::map<std::string, std::shared_ptr<BaseDrawableType>> _drawables;
     };
 
 }
